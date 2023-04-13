a562e81f8ee8eb944be3c2e6593412a17383030e
Dmytro <dmitrymusiichenko@gmail.com>
2023-04-13 11:28:06 +0300

    upd: file structure

--- lab3.2/sections/sidebar.php
+++
@@ -1,7 +0,0 @@
-<h1>Sidebar</h1>
-
-<ul>
-    <li><a href="/practice/lab3.2/?task=1">Task 01</a></li>
-    <li><a href="/practice/lab3.2/?task=2">Task 02</a></li>
-    <li><a href="/practice/lab3.2/?task=3">Task 03</a></li>
-</ul>
---
+++ lab3/sections/sidebar.php
@@ -0,0 +1,7 @@
+<h1>Sidebar</h1>
+
+<ul>
+    <li><a href="/practice/lab3/?task=1">Task 01</a></li>
+    <li><a href="/practice/lab3/?task=2">Task 02</a></li>
+    <li><a href="/practice/lab3/?task=3">Task 03</a></li>
+</ul>
