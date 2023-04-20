186f1548b05d14dd7e8eeaeec68252f23373ebd9
Dmytro <dmitrymusiichenko@gmail.com>
2023-04-20 12:10:09 +0300

    refactor: reduce logic

--- labs/sections/sidebar.php
+++ labs/sections/sidebar.php
@@ -1,18 +1,20 @@
 <h1>Sidebar</h1>
 
-<ul>
-    <?php
-      $fileList = glob('sections/tasks/*');
+<nav>
+  <ul class="list">
+      <?php
+        $fileList = glob('sections/tasks/*');
 
-      foreach($fileList as $filename){
-      if ($filename === 'sections/tasks/task-no-found.php') continue;
-      if(is_file($filename)){
-        $number_of_tasks = preg_replace("/[^0-9]/", "",$filename);
+        foreach($fileList as $filename){
+        if ($filename === 'sections/tasks/task-no-found.php') continue;
+        if(is_file($filename)){
+          $number_of_tasks = preg_replace("/[^0-9]/", "",$filename);
 
-        if($number_of_tasks) {
-          echo "<li><a href='/practice/labs/?task=$number_of_tasks'>Task $number_of_tasks</a></li>";
+          if($number_of_tasks) {
+            echo "<li><a href='/practice/labs/?task=$number_of_tasks'>Task $number_of_tasks</a></li>";
+          }
         }
-      }
-      }
-?>
-</ul>
+        }
+  ?>
+  </ul>
+</nav>
