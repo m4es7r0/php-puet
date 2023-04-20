d4b2ec31ec6a3c2dc3afbbe93738f363a4691379
Dmytro <dmitrymusiichenko@gmail.com>
2023-04-20 10:36:22 +0300

    chore: reduce logic of tasks

--- labs/sections/content.php
+++ labs/sections/content.php
@@ -11,48 +11,20 @@
     // ];  
     
     // var_dump($arr);
-
-
     // print_r($_GET);
-
-    // http://localhost/ppvz-2023/practice-03/?task=1
 
     if (isset($_GET['task'])) {
         $task = filter_var($_GET['task'], FILTER_SANITIZE_NUMBER_INT);        
+        
+        $filePath = dirname(__FILE__) . "/tasks/task-{$task}.php";
 
-        // echo $task . '<br>';
-        // echo $_GET['task'];
-
-
-        $filePath = "tasks/task-{$task}.php";
-        // // TODO необхідно перевірити файл на наявність : DONE
-
-        // // не працюе file_exist  ??? 
-        // if(file_exists($filePath)) {
-        //     include($filePath);
-        // }
-
-        switch ($task) {
-            case 1:
-                include($filePath);
-                break;
-            case 2:
-                include($filePath);
-                break;
-            case 3:
-                include($filePath);
-                break;
-            default:
-                include('tasks/task-no-found.php');     
+        if ($task && file_exists($filePath)) {
+            include($filePath);
+        } else {
+            include('tasks/task-no-found.php');  
         }
+
     } else {
         echo 'Key task is not defined!';
     }
-
-    // switch () {
-    //     case '1':
-    //         break;
-
-    //     default:
-    // }
     
--- labs/sections/sidebar.php
+++ labs/sections/sidebar.php
@@ -1,7 +1,18 @@
 <h1>Sidebar</h1>
 
 <ul>
-    <li><a href="/practice/labs/?task=1">Task 01</a></li>
-    <li><a href="/practice/labs/?task=2">Task 02</a></li>
-    <li><a href="/practice/labs/?task=3">Task 03</a></li>
+    <?php
+      $fileList = glob('sections/tasks/*');
+
+      foreach($fileList as $filename){
+      if ($filename === 'sections/tasks/task-no-found.php') continue;
+      if(is_file($filename)){
+        $number_of_tasks = preg_replace("/[^0-9]/", "",$filename);
+
+        if($number_of_tasks) {
+          echo "<li><a href='/practice/labs/?task=$number_of_tasks'>Task $number_of_tasks</a></li>";
+        }
+      }
+      }
+?>
 </ul>
