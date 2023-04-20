7daad93188ba0aa58d1cca6571975ca7b512790e
Dmytro <dmitrymusiichenko@gmail.com>
2023-04-20 09:01:41 +0300

    refactor: rename href in links

--- labs/sections/sidebar.php
+++ labs/sections/sidebar.php
@@ -1,7 +1,7 @@
 <h1>Sidebar</h1>
 
 <ul>
-    <li><a href="/practice/lab3/?task=1">Task 01</a></li>
-    <li><a href="/practice/lab3/?task=2">Task 02</a></li>
-    <li><a href="/practice/lab3/?task=3">Task 03</a></li>
+    <li><a href="/practice/labs/?task=1">Task 01</a></li>
+    <li><a href="/practice/labs/?task=2">Task 02</a></li>
+    <li><a href="/practice/labs/?task=3">Task 03</a></li>
 </ul>
